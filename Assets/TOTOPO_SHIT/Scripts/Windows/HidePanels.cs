6ebf60473d3922d42d71e07e3655850b00600449
David <elchicoconinsomnio@gmail.com>
2016-08-30 00:22:12 +0200

    last fixes

---
+++ Assets/TOTOPO_SHIT/Scripts/Windows/HidePanels.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HidePanels : MonoBehaviour
+{
+    public GameObject[] _panels;
+    public float _timeToLive = 6f;
+    
+    private void OnEnable ()
+    {
+        StartCoroutine(DisablePanels());
+    }
+
+    private void OnDisable ()
+    {
+        foreach (var panel in _panels)
+        {
+            panel.gameObject.SetActive(true);
+        }
+    }
+
+    private IEnumerator DisablePanels ()
+    {
+        yield return new WaitForSeconds(_timeToLive);
+        foreach (var panel in _panels)
+        {
+            panel.gameObject.SetActive(false);
+        }
+    }
+}
