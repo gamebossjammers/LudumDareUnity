87eb1385906e55802358f801d5e400d1641f79db
David <elchicoconinsomnio@gmail.com>
2016-09-03 14:13:41 +0200

    button content animation on click and release

---
+++ Assets/TOTOPO_SHIT/Scripts/UI/ButtonTextAnim.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTextAnim : MonoBehaviour
+{
+    public GameObject _text;
+    public float _displacement = 0.4f;
+
+    public void Click ()
+    {
+        _text.transform.localPosition -= new Vector3 (0, _displacement, 0);
+    }
+
+    public void Release ()
+    {
+        _text.transform.localPosition += new Vector3(0, _displacement, 0);
+    }
+}
